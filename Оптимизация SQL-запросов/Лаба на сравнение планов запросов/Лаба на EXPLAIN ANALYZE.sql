3a4a7a2bf813d8a77c607aa4b587c88590157647
IvanShabunin379 <IvanShabunin379@yandex.ru>
2025-04-11 01:00:21 +0300

    fix folders/files structure and add task conditions

--- Оптимизация SQL-запросов/Лаба на сравнение планов запросов/Лаба на EXPLAIN ANALYZE.sql
+++ Оптимизация SQL-запросов/Лаба на сравнение планов запросов/Лаба на EXPLAIN ANALYZE.sql
@@ -13,10 +13,27 @@ FROM tournaments;
 
 
 
+
 -- Подправить 24
 
 
 
+
+-- 27. Выбрать количество пользователей однофамильцев-тезок среди пользователей
+
+EXPLAIN ANALYZE
+SELECT COUNT(DISTINCT U1.id)
+FROM users U1 JOIN users U2 
+	ON U1.first_name = U2.first_name AND U1.last_name = U2.last_name
+WHERE U1.id <> U2.id;
+
+EXPLAIN ANALYZE
+SELECT SUM(NC.namesakes_count)
+FROM (SELECT first_name, last_name, COUNT(id) AS namesakes_count
+	  FROM users
+	  GROUP BY first_name, last_name
+	  HAVING (COUNT(id) > 1)
+	 ) AS NC;
 
 -- 41. Выбрать все данные о пользователях ни разу не делавших ставки.
 
@@ -77,6 +94,7 @@ WHERE tournaments_count = (SELECT COUNT(T.id)
 
 -- 52. Выбрать пары пользователей, сделавших ставки на одинаковую общую сумму.
 
+EXPLAIN ANALYZE
 WITH total_bets_amount_by_user AS (
 	SELECT U.id AS id,
 		   U.login AS login,
@@ -91,13 +109,25 @@ FROM total_bets_amount_by_user AM1 JOIN total_bets_amount_by_user AM2
 	ON AM1.id != AM2.id AND AM1.total_bets_amount = AM2.total_bets_amount
 WHERE AM1.id < AM2.id;
 
------------------------- переделать с CTE
-SELECT U1.login AS user1,
-	   U2.login AS user2
-FROM users U1 
-	JOIN users U2 ON U1.id != U2.id
-	JOIN bets B1 ON U1.id = B1.user_id
-	JOIN bets B2 ON U2.id = B2.user_id
-WHERE U1.id < U2.id
-GROUP BY user1, user2
-HAVING SUM(B1.amount) = SUM(B2.amount);
+
+EXPLAIN ANALYZE
+SELECT U1.login AS login1,
+       U2.login AS login2
+FROM (
+    SELECT U.id,
+           U.login,
+           SUM(B.amount) AS total_bets_amount
+    FROM users U 
+    JOIN bets B ON U.id = B.user_id
+    GROUP BY U.id, U.login
+) AS U1
+JOIN (
+    SELECT U.id,
+           U.login,
+           SUM(B.amount) AS total_bets_amount
+    FROM users U 
+    JOIN bets B ON U.id = B.user_id
+    GROUP BY U.id, U.login
+) AS U2
+ON U1.total_bets_amount = U2.total_bets_amount
+WHERE U1.id < U2.id;
